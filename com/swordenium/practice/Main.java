e1e0ce3b112dcfdab124c96037c1fc304a0e3a75
Mohammed Saad Yaseen <m.saad.yaseen@gmail.com>
2022-11-04 23:11:53 -0400

    Experimenting keyword var which is only applicable to local variables, the compiler decides the type based on the value assigned to the variable.

---
+++ com/swordenium/practice/Main.java
@@ -0,0 +1,16 @@
+package com.swordenium.practice;
+
+public class Main {
+    public static void main(String[] args) {
+        var number = 10;
+        var word = "String";
+
+        /*
+        It allows programmers to declare the type of the local variable instead of the actual type
+         and increases code readability.
+         The compiler decides the type based on the value assigned to the variable.
+        * **/
+        System.out.println(number);
+        System.out.println(word);
+    }
+}
